9bf87dbaf98dc9d0d7923f13aacacd5924057526
ojjasvi-jain <85935892+ojjasvi-jain@users.noreply.github.com>
2023-01-13 21:33:19 +0530

    Add files via upload
    added files

---
+++ index.js
@@ -0,0 +1,39 @@
+let progress = document.querySelector(".progress");
+let nextButton = document.getElementById("next");
+let prevButton = document.getElementById("prev");
+let circles = document.querySelectorAll(".circle");
+
+let activeCircle = 1;
+
+nextButton.addEventListener("click", () => {
+  activeCircle++;
+  if (activeCircle >= circles.length) activeCircle = circles.length;
+  updates();
+});
+
+prevButton.addEventListener("click", () => {
+  activeCircle--;
+  if (activeCircle === 1) activeCircle = 1;
+
+  updates();
+});
+
+const updates = () => {
+  circles.forEach((circle, index) => {
+    if (activeCircle > index) circle.classList.add("active");
+    else circle.classList.remove("active");
+  });
+
+  let activeButton = document.querySelectorAll(".active").length;
+
+  progress.style.width =
+    ((activeButton - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (activeButton === circles.length) nextButton.disabled = true;
+  else if (activeButton === 1) {
+    prevButton.disabled = true;
+  } else {
+    nextButton.disabled = false;
+    prevButton.disabled = false;
+  }
+};
