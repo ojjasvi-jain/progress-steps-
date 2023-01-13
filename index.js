f07433164254937427c3665ffd5d62910f2911b4
ojjasvi-jain <85935892+ojjasvi-jain@users.noreply.github.com>
2023-01-13 23:15:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let progress = document.querySelector(".progress");
 let nextButton = document.getElementById("next");
 let prevButton = document.getElementById("prev");
 let circles = document.querySelectorAll(".circle");
+let smile = document.querySelector(".smile");
 
 let activeCircle = 1;
 
@@ -26,9 +27,17 @@ const updates = () => {
 
   let activeButton = document.querySelectorAll(".active").length;
 
+  smile.style.left =
+    ((activeButton - 1) / (circles.length - 1)) * 100 -
+    3 * (activeButton - 1) +
+    "%";
+
   progress.style.width =
     ((activeButton - 1) / (circles.length - 1)) * 100 + "%";
-
+  console.log(
+    ((activeButton - 1) / (circles.length - 1)) * 100 - 2 * (activeButton - 1)
+  );
+  console.dir(smile);
   if (activeButton === circles.length) nextButton.disabled = true;
   else if (activeButton === 1) {
     prevButton.disabled = true;
